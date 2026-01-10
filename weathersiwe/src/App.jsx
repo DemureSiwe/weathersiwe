b828d024db9fb1225ccd32d49af2287d89f8aa8e
DemureBusi <letsetse.b@gmail.com>
2026-01-10 04:44:42 +0200

    initialized with basic app structure

---
+++ weathersiwe/src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Weather from "./Weather";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="Johannesburg" />
+
+        <footer>
+          This project is open sourced on{" "}
+          <a
+            href="https://github.com/DemureSiwe/weatherreact.git"
+            target="blank"
+          >
+            Github
+          </a>{" "}
+          by Busisiwe Tsetseng
+        </footer>
+      </div>
+    </div>
+  );
+}
