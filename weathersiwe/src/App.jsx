c611c3fd2d08e7d99391e14662e467fc03e03386
DemureBusi <letsetse.b@gmail.com>
2026-02-06 05:52:39 +0200

    added background image and made minor chages to app.css

--- weathersiwe/src/App.jsx
+++ weathersiwe/src/App.jsx
@@ -1,37 +1,38 @@
 import Weather from "./Weather";
 import "bootstrap/dist/css/bootstrap.min.css";
-import weathersiwebg from "./assets/Image/weathersiwebg.jpg";
+import weathersiwe2 from "./assets/weathersiwe2.jpg";
 import "./App.css";
 
 export default function App() {
   const coverStyle = {
-    backgroundImage: `url(${weathersiwebg})`,
+    backgroundImage: `url(${weathersiwe2})`,
     backgroundSize: "cover",
+    backgroundPosition: "center",
     backgroundRepeat: "no-repeat",
-    height: "100px",
-    width: "100 %",
-    opacity,
+    height: "100vh",
   };
 
   return (
-    <div className="App">
-      <div className="container">
-        <Weather defaultCity="Johannesburg" />
+    <section style={coverStyle}>
+      <div className="App">
+        <div className="container">
+          <Weather defaultCity="Johannesburg" />
 
-        <footer>
-          This project is open sourced on{" "}
-          <a
-            href="https://github.com/DemureSiwe/weatherreact.git"
-            target="blank"
-          >
-            Github
-          </a>{" "}
-          by Busisiwe Tsetseng and hosted on{" "}
-          <a href="https://reactweather-siwe.netlify.app/" target="blank">
-            Netlify
-          </a>
-        </footer>
+          <footer>
+            This project is open sourced on{" "}
+            <a
+              href="https://github.com/DemureSiwe/weatherreact.git"
+              target="blank"
+            >
+              Github
+            </a>{" "}
+            by Busisiwe Tsetseng and hosted on{" "}
+            <a href="https://reactweather-siwe.netlify.app/" target="blank">
+              Netlify
+            </a>
+          </footer>
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
